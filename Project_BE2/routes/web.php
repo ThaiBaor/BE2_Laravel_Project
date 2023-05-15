589767b4ec60840cd3f1d9147a777b0fa39a15b3
ThaiBaor <ngphanthaibao203@gmail.com>
2023-05-15 17:40:55 +0700

    rollback

--- Project_BE2/routes/web.php
+++ Project_BE2/routes/web.php
@@ -68,8 +68,15 @@ Route::get('/test', FUNCTION () {
 
 
 
-
-
+// Category
+Route::get('listcategory', [CategoryController:: class, 'listCategory']) -> name('listcategory');
+Route::get('addcategory', [CategoryController::class, 'addcategory'])->name('addcategory');
+Route::post('customcategory', [CategoryController::class, 'customCategory'])->name('customcategory.custom');
+Route::get('getdataedtcategory/id{id}', [CategoryController::class, 'getDataEditCategory'])->name('getdataedtcategory');
+Route::post('editcategory', [CategoryController::class, 'updateCategory'])->name('editcategory');
+Route::get('deletecategory/id{id}', [CategoryController::class, 'deleteCategory'])->name('deletecategory');
+Route::get('searchcategory', [CategoryController::class, 'searchCategory'])->name('searchcategory');
+//---------
 
 
 
