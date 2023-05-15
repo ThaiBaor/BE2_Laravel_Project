5e1a212b6aca0321be566130e2986c94c728ff10
hoangtrong203 <tronglee2003@gmail.com>
2023-05-16 01:30:33 +0700

    shop by category

---
+++ Project_BE2/app/Http/Controllers/ShopByCategory.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+use DB;
+use Illuminate\Http\Request;
+
+class ShopByCategoryController extends Controller
+{
+    public function getAllProducts()
+    {
+        $products = DB::table('products')->select('*')->where('id_category', $id)->get();
+        return view('shopbycategory', compact('products'));
+    }
+    
+}
