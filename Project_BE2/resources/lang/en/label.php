50f5c9c209d7129533517aceeb2bcd6097871ac2
ThaiBaor <ngphanthaibao203@gmail.com>
2023-05-17 19:21:54 +0700

    tao folder lang

---
+++ Project_BE2/resources/lang/en/label.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'Welcome'=>'Free shipping, 30-day return or refund guarantee.',
+    'login' => 'Login',
+    'logout'=>'Logout',
+    'signup'=>'Sign up',
+    'home' => 'Home',
+    'Shop' => 'Shop',
+    'category' => 'Category'
+]
+?>
---
+++ Project_BE2/resources/lang/vi/label.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'Welcome'=>'Miễn phí vận chuyển. Hoàn trả trong 30 ngày.',
+    'login' => 'Đăng nhập',
+    'logout'=>'Thoát',
+    'signup'=>'Đăng kí',
+    'home' => 'Trang chủ',
+    'Shop' => 'Sản phẩm',
+    'category' => 'Loại'
+]
+?>
